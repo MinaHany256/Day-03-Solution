d6245fecfdb5d2670351ec56dc91e7acc59b363a
MinaHany256 <minahanyfoad@gmail.com>
2024-06-26 15:02:43 +0300

    Part 01 Conditional Statments - If, Switch [Numeric Datatypes]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main()
         {
-           
+            #region Control Statment  1. Conditional Satement [If - Switch]
+
+            #region Example 01 [Numberic Type : Equality]
+            //Console.WriteLine("Please Enter a month Numbers Existed in 1st Qurater: ");
+            //int.TryParse(Console.ReadLine(), out int MonthNumber);
+
+            //if ( MonthNumber == 1 )
+            //{
+            //    Console.WriteLine("Hello January");
+            //} 
+            //else if ( MonthNumber == 2 )
+            //{
+            //    Console.WriteLine("Hello February");
+            //}
+            //else if( MonthNumber == 3 )
+            //{
+            //    Console.WriteLine("Hello March");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You Entered a month not in the 1st Quarter");
+            //}
+
+            //switch (MonthNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Hello January");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Hello February");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Hello March");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You Entered a month not in the 1st Quarter");
+            //        break;
+
+            //} 
+            #endregion
+
+
+
+            #endregion
+
+
         }
     }
 }
