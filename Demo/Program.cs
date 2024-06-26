d24ae96d422c42d90182ebc286cbfcf321b78be5
MinaHany256 <minahanyfoad@gmail.com>
2024-06-26 15:20:17 +0300

    Part 02 Switch [Numeric Datatypes (Comparsion Operators)]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -45,6 +45,37 @@
             //} 
             #endregion
 
+            #region Exampe 02 [Numeric Type : Comparison]
+            //Console.WriteLine("Please Enter Your Age : ");
+            //int.TryParse(Console.ReadLine(), out int Age);
+
+            //if (Age > 22)
+            //{
+            //    Console.WriteLine("Your Age is Greater Than 22");
+            //}
+            //else if (Age < 22)
+            //{
+            //    Console.WriteLine("Your Age is Less Than 22");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your Age is 22");
+            //}
+
+
+            //switch(Age)
+            //{
+            //    case > 22:
+            //        Console.WriteLine("Your Age is Greater Than 22");
+            //        break;
+            //    case < 22:
+            //        Console.WriteLine("Your Age is Less Than 22");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Your Age is 22");
+            //        break;
+            //} 
+            #endregion
 
 
             #endregion
