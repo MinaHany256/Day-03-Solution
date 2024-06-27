31b984c3c050781961ee5777ce80a8e0a436d005
MinaHany256 <minahanyfoad@gmail.com>
2024-06-27 20:08:37 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -15,6 +15,17 @@
 
             #endregion
 
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+            //Console.Write("Please Enter a Number : ");
+            //int.TryParse(Console.ReadLine(), out int number);
+
+            //if(number < 0)
+            //    Console.WriteLine("Negative");
+            //else
+            //    Console.WriteLine("Postitive");
+            #endregion
+
         }
     }
 }
