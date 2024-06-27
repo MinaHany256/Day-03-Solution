0755820c9fb06b320c72683974978b97c960dce9
MinaHany256 <minahanyfoad@gmail.com>
2024-06-27 20:19:38 +0300

    Question 7

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -106,6 +106,16 @@
 
             #endregion
 
+            #region 7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+            //Console.Write("Please Enter any Number : ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i < 12; i++) 
+            //    Console.Write($" {number * i} ");
+            #endregion
+
+
         }
     }
 }
