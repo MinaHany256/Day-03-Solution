2003951ddea3ab8259ed39f38684733567b8505d
MinaHany256 <minahanyfoad@gmail.com>
2024-06-27 20:07:26 +0300

    Question 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main()
         {
-            
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+            //Console.Write("Please Enter a Number can be divided By 3 and 4 : ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //if (number % 3 == 0 && number % 4 ==0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("no");
+
+            #endregion
+
         }
     }
 }
